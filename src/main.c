7b8dd2d4e8db2d972ba8581f628198acfe175bff
Ian Moffett <teaqllabs@gmail.com>
2021-12-24 19:46:08 -0500

    Fixed blank line hang bug.

--- src/main.c
+++ src/main.c
@@ -38,11 +38,15 @@ int main(int argc, char* argv[]) {
 
     init_tokenlist(&toklist);
 
-    unsigned long lbridx = 0;
+    unsigned long lbridx = 0;  // Line buffer release index.
     unsigned int curIdx = 0;
-	unsigned long count = 0;
+	unsigned long count = 0;  // This value fixed sefault.
 
     while (getline(&lineBuf, &lineBufSize, fp) != -1 && !(lexer.error)) {
+		if (strlen(lineBuf) - 1 == 0) {
+			continue;
+		}
+
         while (curIdx < strlen(lineBuf) - 2) {
             for (curIdx; curIdx < strlen(lineBuf); ++curIdx) {
                 lineBufRelease[lbridx] = lineBuf[curIdx];
@@ -68,7 +72,7 @@ int main(int argc, char* argv[]) {
         ++lexer.lineNum;
     }
 
-    if (lexer.error) {
+    if (lexer.error) { 
         fclose(fp);
         destroy_tokenlist(&toklist);
         return -1;
@@ -81,7 +85,12 @@ int main(int argc, char* argv[]) {
 
 	size_t nodelistSize = 0;
 
-	for (int i = 0; i < toklist.elements - count; ++i) {
+	for (int i = 0; i < toklist.elements - count; ++i) { 
+		/*
+		 * Fixed segfault by subtracting element count by line count.
+		 * No idea how that fixed it.
+		 */
+
 		struct AST_NODE** ast = parse(&parser, &nodelistSize);
 		execute(ast);
 		ast_destroy(&ast, nodelistSize);
